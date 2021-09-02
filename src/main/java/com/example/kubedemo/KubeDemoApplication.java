b3270e89137c2813a797695044864e1ac2ecc233
Richard Stefanca <richard.stefanca@moneta.cz>
2021-09-02 13:40:40 +0200

    Initial commit

---
+++ src/main/java/com/example/kubedemo/KubeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.kubedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubeDemoApplication.class, args);
+    }
+
+}
